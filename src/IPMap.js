adc1d32d5dda59d12389cdc9a28f65dca87991b9
Miguel <arriaga.miguel123@gmail.com>
2021-05-17 13:34:00 -0500

    trigger github action

--- src/IPMap.js
+++ src/IPMap.js
@@ -5,28 +5,25 @@ import iconLocation from './media/icon-location.svg'
 
 const IPMap = ({ data }) => {
 
+    /* coordinates */
+    let coordinates = [data.location.lat, data.location.lng]
 
-    let center = [data.location.lat, data.location.lng]
-
+    /* add a custom icon for target */
     let icon = L.icon({
         iconUrl: iconLocation,
         iconRetinaUrl: iconLocation,
         iconAnchor: null,
         popupAnchor: [0,-20],
         iconSize: [30, 35],
-    });
-
-    React.useEffect(() => {
-
-    }, [])
+    })
 
     return (
-        <MapContainer center={center} zoom={12} scrollWheelZoom={false} id="map" boundsOptions={{paddingBottomRight: [250, 0]}}>
+        <MapContainer center={coordinates} zoom={12} scrollWheelZoom={false} id="map" boundsOptions={{paddingBottomRight: [250, 0]}}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={center} icon={icon} >
+            <Marker position={coordinates} icon={icon} >
                 <Popup>
                     {`${data.ip}`}
                 </Popup>
