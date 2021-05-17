cff5e687c8227d9d062fb927091aee16855ec6c8
Miguel <arriaga.miguel123@gmail.com>
2021-05-17 13:53:22 -0500

    add leaflet to package.json

--- src/IPMap.js
+++ src/IPMap.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import L from 'leaflet'
 import iconLocation from './media/icon-location.svg'
 
